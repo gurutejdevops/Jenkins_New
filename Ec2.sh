af0b8b424e83d3001514c973fbf7bed4c0facd6b
Guru Theja <gurutejashetty@gmail.com>
2026-02-15 11:44:52 +0530

    auto-push

--- Ec2.sh
+++ Ec2.sh
@@ -31,7 +31,6 @@ INSTANCE_ID=$(aws ec2 run-instances \
   --image-id $AMI_ID \
   --count 1 \
   --instance-type $INSTANCE_TYPE \
-# --key-name $KEY_NAME \
   --security-group-ids $SECURITY_GROUP \
   --subnet-id $SUBNET_ID \
   --region $REGION \
